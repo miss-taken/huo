2d3ae82582af6cbe4256fa0aefeb27a73553eacb
duian <zhouqiao0418@gmail.com>
2016-09-06 16:39:39 +0800

    feat: 登录

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,17 +1,72 @@
 import React, { Component } from 'react';
-import { Icon } from 'antd-mobile';
+import { Link } from 'react-router';
+import { createForm } from 'rc-form';
+import { Icon, List, InputItem, Button, Toast, WingBlank } from 'antd-mobile';
+import './_login';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit() {
+    const { form } = this.props;
+    const { username = '', password = '' } = form.getFieldsValue();
+    Toast.success(`成功提交账号信息, ${username} ${password}`);
+  }
+
   render() {
-    console.log('entry');
+    const { getFieldProps } = this.props.form;
     return (
-      <div>
-        hello world
-        <span><Icon type="lock"/></span>
-        <Icon type="mobile"/>
+      <div className="login">
+        <div className="login-bg">
+          <h3 className="logo">货管家</h3>
+            <div className="login-menu">
+              <Link to="register" activeClassName="active">注册</Link>
+              <Link to="login" activeClassName="active">登录</Link>
+            </div>
+        </div>
+        <div className="login-body">
+          <List>
+            <List.Body>
+              <InputItem
+                {...getFieldProps('username')}
+                className="form-mobile"
+                placeholder="请输入手机号"
+                labelNumber={2}
+                type="number"
+                clear
+                maxLength={11}
+              >
+                <Icon type="mobile"/>
+              </InputItem>
+              <InputItem
+                {...getFieldProps('password')}
+                className="form-password"
+                placeholder="请输入密码"
+                type="password"
+                labelNumber={2}
+                clear
+              >
+                <Icon type="lock"/>
+              </InputItem>
+              <WingBlank>
+                <Button
+                  className="login-submit"
+                  type="warning"
+                  onClick={this.handleSubmit}>
+                  确定
+                </Button>
+              </WingBlank>
+            </List.Body>
+          </List>
+        </div>
       </div>
     );
   }
 }
 
-export default Login;
+const _Login = createForm()(Login);
+export default _Login;
